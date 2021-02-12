266556331e4db82b6e8304a7754986bd07cc3946
John Bayly <tipstrade@users.noreply.github.com>
2021-02-12 13:58:41 +0000

    Fix CheckType values

--- TipsTrade.VCheck/Enumerations.cs
+++ TipsTrade.VCheck/Enumerations.cs
@@ -2,10 +2,10 @@
   /// <summary>The list of possible report check types.</summary>
   public enum CheckType {
     /// <summary>An initial check.</summary>
-    Initial = 1,
+    Initial = 2,
     /// <summary>A basic check.</summary>
-    Basic = 2,
+    Basic = 3,
     /// <summary>A full check.</summary>
-    Full = 3
+    Full = 4
   }
 }
